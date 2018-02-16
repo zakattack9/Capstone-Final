2e2517bd40dd11e869f7c88a882cb268d5ca8309
Aoiryuu <19joroka@waipahuh.k12.hi.us>
2018-02-16 09:26:06 -1000

    adding working polly

--- lambdas/handler.js
+++ lambdas/handler.js
@@ -39,44 +39,43 @@ module.exports.hello = (event, context, callback) => {
   })
 };*/
 
-module.exports.hello = (event, context, callback) => {
-  // Load the SDK
-  const AWS = require('aws-sdk')
-  const Stream = require('stream')
-  const Speaker = require('speaker')
+// Load the SDK
+const AWS = require('aws-sdk');
+const Stream = require('stream');
+const Speaker = require('speaker');
 
-  // Create an Polly client
-  const Polly = new AWS.Polly({
-    signatureVersion: 'v4',
-    region: 'us-west-2'
-  })
+// Create an Polly client
+const Polly = new AWS.Polly({
+  signatureVersion: 'v4',
+  region: 'us-west-2'
+})
 
-  // Create the Speaker instance
-  const Player = new Speaker({
-    channels: 1,
-    bitDepth: 16,
-    sampleRate: 16000
-  })
+// Create the Speaker instance
+const Player = new Speaker({
+  channels: 1,
+  bitDepth: 16,
+  sampleRate: 16000
+})
 
-  let params = {
-      'Text': 'Hi, my name is @anaptfox.',
-      'OutputFormat': 'pcm',
-      'VoiceId': 'Kimberly'
-  }
-
-  Polly.synthesizeSpeech(params, (err, data) => {
-      if (err) {
-          console.log(err.code)
-      } else if (data) {
-          if (data.AudioStream instanceof Buffer) {
-              // Initiate the source
-              var bufferStream = new Stream.PassThrough()
-              // convert AudioStream into a readable stream
-              bufferStream.end(data.AudioStream)
-              // Pipe into Player
-              bufferStream.pipe(Player)
-          }
-      }
-  })
+let params = {
+    'Text': 'Jay do your work',
+    'OutputFormat': 'pcm',
+    'VoiceId': 'Nicole'
 }
 
+Polly.synthesizeSpeech(params, (err, data) => {
+  console.log("ERROR", err);
+  console.log("DATA", data);
+  if (err) {
+      console.log(err.code)
+  } else if (data) {
+      if (data.AudioStream instanceof Buffer) {
+          // Initiate the source
+          var bufferStream = new Stream.PassThrough()
+          // convert AudioStream into a readable stream
+          bufferStream.end(data.AudioStream)
+          // Pipe into Player
+          bufferStream.pipe(Player)
+      }
+  }
+})
